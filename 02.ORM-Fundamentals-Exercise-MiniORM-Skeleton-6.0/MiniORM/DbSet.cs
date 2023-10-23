4e6547eaf4ed358dbd407e7e85f9d0d8d1c862a9
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-10-23 23:58:11 +0300

    Update DbSet.cs

--- 02.ORM-Fundamentals-Exercise-MiniORM-Skeleton-6.0/MiniORM/DbSet.cs
+++ 02.ORM-Fundamentals-Exercise-MiniORM-Skeleton-6.0/MiniORM/DbSet.cs
@@ -1,7 +1,48 @@
-﻿namespace MiniORM
+﻿using System.Collections;
+
+namespace MiniORM
 {
-    public class DbSet
+    public class DbSet<TEntity> : ICollection<TEntity>
+        where TEntity : class, new()
     {
-        // TODO: Create your DbSet class here.
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(TEntity[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
