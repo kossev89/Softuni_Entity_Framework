f2f704098b8b4a481aa5765a5cbf7ac9d1c3b05a
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-11-16 18:22:57 +0200

    Update StartUp.cs

--- 05.LINQ-Exercises/05.LINQ-Exercises-MusicHub-6.0/MusicHub/StartUp.cs
+++ 05.LINQ-Exercises/05.LINQ-Exercises-MusicHub-6.0/MusicHub/StartUp.cs
@@ -4,6 +4,8 @@
     using System.Text;
     using Data;
     using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
 
     public class StartUp
     {
@@ -13,33 +15,34 @@
                 new MusicHubDbContext();
 
             DbInitializer.ResetDatabase(context);
+            int duration = 4;
+            Console.WriteLine(ExportSongsAboveDuration(context, duration));
 
-            int producerId = 9;
-            Console.WriteLine(ExportAlbumsInfo(context, producerId));
+
         }
 
         public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
         {
             var albums = context.Albums
-             .Where(p=>p.ProducerId==producerId)
+             .Where(p => p.ProducerId == producerId)
              .Select(e => new
              {
-                    e.Name,
-                    e.ReleaseDate,
-                    ProducerName=e.Producer.Name,
-                    e.Price,
-                    Songs=e.Songs
-                    .Select(s=> new
+                 e.Name,
+                 e.ReleaseDate,
+                 ProducerName = e.Producer.Name,
+                 e.Price,
+                 Songs = e.Songs
+                    .Select(s => new
                     {
                         s.Name,
                         s.Price,
-                        WriterName=s.Writer.Name
+                        WriterName = s.Writer.Name
                     })
-                    .OrderByDescending(s=>s.Name)
-                    .ThenBy(w=>w.WriterName)
+                    .OrderByDescending(s => s.Name)
+                    .ThenBy(w => w.WriterName)
                     .ToList()
              })
-             .OrderByDescending(s=>s.Songs.Sum(x=>x.Price))
+             .OrderByDescending(s => s.Songs.Sum(x => x.Price))
              .ToList();
 
             StringBuilder output = new();
@@ -66,8 +69,49 @@
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
         {
-            throw new NotImplementedException();
+            TimeSpan durationInSeconds = TimeSpan.FromSeconds(duration);
+            var songs = context.Songs
+                .Where(x => x.Duration > durationInSeconds)
+                .Select(e => new
+                {
+                    e.Name,
+                    Performers = e.SongPerformers
+                    .Select(sp => new
+                    {
+                        sp.Performer.FirstName,
+                        sp.Performer.LastName
+                    })
+                    .OrderBy(fn => fn.FirstName)
+                    .ThenBy(ln=>ln.LastName)
+                    .ToList(),
+                    WriterName = e.Writer.Name,
+                    ProducerName = e.Album.Producer.Name,
+                    e.Duration
+                })
+                .OrderBy(n => n.Name)
+                .ThenBy(wn => wn.WriterName)
+                .ToList();
 
+            StringBuilder sb = new();
+            int songNumber = 0;
+
+            foreach (var s in songs)
+            {
+                songNumber++;
+                sb.AppendLine($"-Song #{songNumber}");
+                sb.AppendLine($"---SongName: {s.Name}");
+                sb.AppendLine($"---Writer: {s.WriterName}");
+                if (s.Performers.Any())
+                {
+                    foreach (var p in s.Performers)
+                    {
+                        sb.AppendLine($"---Performer: {p.FirstName} {p.LastName}");
+                    }
+                }
+                sb.AppendLine($"---AlbumProducer: {s.ProducerName}");
+                sb.AppendLine($"---Duration: {s.Duration.ToString("c")}");
+            }
+            return sb.ToString().Trim();
         }
     }
 }
