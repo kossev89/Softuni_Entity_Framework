df024abc0ae5321bc710300cbb10f35053bcaf5e
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-11-30 00:36:56 +0200

    Adding Boardgames database

---
+++ ExamPreparation/C# DB Advanced Exam - 01 April 2023/Boardgames/Data/Models/Seller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boardgames.Data.Models
+{
+    public class Seller
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [StringLength(30)]
+        public string Address { get; set; } = null!;
+        [Required]
+        [StringLength(30)]
+        public string Country { get; set; } = null!;
+        [Required]
+        [StringLength(30)]
+        public string Website { get; set; } = null!;
+        public ICollection<BoardgameSeller>? BoardgamesSellers { get; set; } = new HashSet<BoardgameSeller>();
+    }
+}
---
+++ ExamPreparation/C# DB Advanced Exam - 01 April 2023/Boardgames/Utilities/XmlHelper.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Boardgames.Utilities
+{
+    public class XmlHelper
+    {
+        public T Decerialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            using StringReader reader = new StringReader(inputXml);
+            T decerializedDtos = (T)xmlSerializer.Deserialize(reader);
+
+            return decerializedDtos;
+        }
+
+        public string Serialize<T>(T objectToSerialize, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(objectToSerialize.GetType(), xmlRoot);
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+
+            using (StringWriter writer = new StringWriter())
+            {
+                xmlSerializer.Serialize(writer, objectToSerialize, ns);
+
+                return writer.ToString();
+            }
+
+        }
+    }
+}
